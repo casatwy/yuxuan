39542084fea3e693d80236184cba67d3da84b17a
CasaTaloyum <casatwy@msn.com>
2013-01-10 20:44:09 +0800

    added underscore.js

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -32,6 +32,11 @@ class Controller extends CController
                 'baseUrl' => $this->baseUrl.'/js/libs/',
                 'js' => array('jquery-1.8.3.min.js')
             ),
+            'underscore'=>array(
+                'basePath' => 'webroot.js.libs',
+                'baseUrl' => $this->baseUrl.'/js/libs/',
+                'js' => array('underscore-min.js')
+            ),
             'jqueryPlugins' => array(
                 'basePath' => 'webroot.js.libs.plugins',
                 'baseUrl' => $this->baseUrl.'/js/libs/plugins',
@@ -42,6 +47,7 @@ class Controller extends CController
         );
         $this->cs->registerPackage('jquery');
         $this->cs->registerPackage('jqueryPlugins');
+        $this->cs->registerPackage('underscore');
 
         $this->jsUrl = $this->baseUrl.Yii::app()->assetManager->publish(Yii::getPathOfAlias('webroot.js.app.'.$this->id)).'/';
         //$this->jsUrl = $this->baseUrl.Yii::app()->assetManager->getPublishedUrl(Yii::getPathOfAlias('webroot.js.app.'.$this->id), true).'/';
