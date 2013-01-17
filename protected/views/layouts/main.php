d16e01512816144ccb7cfd9be5cdb8ae4e1aad28
chrisbin <chrisbin@chrisbin-HP-Compaq-6531s.(none)>
2013-01-17 12:40:49 +0800

    admininfo

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,6 +32,7 @@
     <div id="mainmenu">
         <?php $this->widget('zii.widgets.CMenu',array(
             'items'=>array(
+                array('label'=>'信息管理', 'url'=>array('/admin')),
                 array('label'=>'仓库', 'url'=>array('/storage')),
                 array('label'=>'生产计划', 'url'=>array('/plan')),
                 array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
