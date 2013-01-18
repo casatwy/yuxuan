ad54c31f184a657281672aaac36562fd3d735717
Casa Taloyum <casatwy@msn.com>
2013-01-18 15:18:14 +0800

    events show finished

--- js/app/plan/localPlan.js
+++ js/app/plan/localPlan.js
@@ -16,19 +16,37 @@ function LocalPlan(baseUrl){
         $("#J_calendar").fullCalendar({
             editable:false,
             theme:true,
+            events:baseUrl + "/ajaxPlan/getPlanEvents",
             header:{
-                left:'前一天，下一天 今天',
-                center:'标题',
-                right:'月，周，日',
+                left:'prev title next',
+                center:'',
+                right:'today'
             },
-            //events:"url to get events, data type is json",
-            dayClick:function(){
+            weekMode:'liquid',
+
+            monthNames:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+            monthNamesShort:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+            dayNames:["周一","周二","周三","周四","周五","周六","周日"],
+            dayNamesShort:["一","二","三","四","五","六","日"],
+
+            dayClick:function(date, allDay, jsEvent, view){
+                //date: Tue Jan 01 2013 00:00:00 GMT+0800 (CST)
+                //allDay: true (boolean)
+                //view: a big object
                 clickOnDay();
             },
+            eventClick:function(){
+                clickOnEvent();
+            }
         });
 
         function clickOnDay(){
-            alert("day Clicked");
+            alert("day clicked");
+        }
+
+        function clickOnEvent(){
+            alert("event clicked");
+            return false;
         }
     }
 }
---
+++ protected/controllers/AjaxPlanController.php
@@ -0,0 +1,35 @@
+<?php
+
+class AjaxPlanController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+
+    public function actionGetPlanEvents(){
+        $events = array(
+            array(
+                'id' => 'event_id_1',
+                'title' => 'event_title_1',
+                'allDay' => true,
+                'start' => time()-(60*60*24),
+                'end' => time(),
+                'className' => 'J_event',
+                'editable' => false,
+            ),
+            array(
+                'id' => 'event_id_2',
+                'title' => 'event_title_2',
+                'allDay' => false,
+                'start' => time()-(60*60*24*2),
+                'end' => time()-(60*60*24),
+                'className' => 'J_event',
+                'editable' => false,
+            ),
+        );
+
+        echo CJSON::encode($events);
+    }
+
+}
