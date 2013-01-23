fea0afbe7236c7112dd775bbc610eb1b2600f4b4
winni <winni@winni-PC.(none)>
2013-01-23 20:21:42 +0800

    printdeliveredplan

--- protected/views/plan/printDeliveredPlan.php
+++ protected/views/plan/printDeliveredPlan.php
@@ -1,4 +1,13 @@
-<div class="contant-container" >
+<div class="container font-2">
+	<h1 class="center big">上海星位针织服装有限公司<h1>
+	<h2 class="center">XXXXXXX</h2>
+
+	<span class="span-16 prepend-1" >客户：<span>xxxxxx</span></span>
+	<span class="span-7 last">单据号：<span>xxxxxx</span></span>
+	<span class="span-16 prepend-1">摘要：<span>xxxxxxxxx</span></span>
+	<span class="span-7 last"><span></span></span>
+	<span class="span-16 prepend-1"><span></span></span>
+	<span class="span-7 last small"></span>
 <table class="record">
 
     <tr>
@@ -20,4 +29,22 @@
     </tr>
     <?php endforeach;?>
 </table>
+	<span class="span-23 last prepend-1">如有疑问，请你回传给我们！
+		<span>11111111111</span>&nbsp;或&nbsp;
+		<span>11111111111111111111</span>
+	</span>
+
+<span class="span-5">制单人：<span>1111111</span></span>
+<span class="span-5">发货人：<span>11111111</span></span>
+<span class="span-5">审核人：<span>1111111</span></span>
+<span class="span-5">进货人：<span>111111</span></span>
+<span class="span-4 last">客户签收：<span>11111111</span></span>
+<span class="span-5">制单日期：<span>1111111</span></span>
+<span class="span-5">发货日期：<span></span></span>
+<span class="span-5">审核日期：<span></span></span>
+<span class="span-5">进货日期：<span></span></span>
+<span class="span-4 last">签收日期：<span></span></span>
+
+</div>
+
 </div>
