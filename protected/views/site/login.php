4ed632f480ed861e3a272861c2bf12510cfaef37
winni <winni@winni-PC.(none)>
2013-01-24 22:47:23 +0800

    login

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -6,8 +6,9 @@
 
 $this->pageTitle=Yii::app()->name . ' - 登录';
 ?>
-<div class="prepend-3">
-<h1>登录</h1>
+<div class="prepend-12 log-bg" >
+<div id="login">
+<h1>LOG IN</h1>
 
 <div class="form">
 <?php $form=$this->beginWidget('CActiveForm', array(
@@ -19,19 +20,20 @@ $this->pageTitle=Yii::app()->name . ' - 登录';
 )); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($loginForm,'username'); ?>
-		<?php echo $form->dropDownList($loginForm,'username', $nameList); ?>
+		<span class="prepend-1 span-2"><?php echo $form->labelEx($loginForm,'username'); ?></span>
+		<span class="span-3 last"><?php echo $form->dropDownList($loginForm,'username', $nameList); ?></span>
 	</div>
 
 	<div class="row">
-		<?php echo $form->labelEx($loginForm,'password'); ?>
-		<?php echo $form->passwordField($loginForm,'password'); ?>
+		<span class="prepend-1 span-2"><?php echo $form->labelEx($loginForm,'password'); ?></span>
+		<span class="span-3 last"><?php echo $form->passwordField($loginForm,'password'); ?></span>
 	</div>
 
-	<div class="row buttons">
+	<div class="row buttons prepend-3 span-3 last">
 		<?php echo CHtml::submitButton('Login'); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
 </div><!-- form -->
+</div>
 </div>
