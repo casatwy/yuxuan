fd648aa4531bfd933ef6e590d6d22a9b98242266
winni <winni@winni-PC.(none)>
2013-02-08 22:47:06 +0800

    js-test

--- js/common/bigtable.js
+++ js/common/bigtable.js
@@ -11,6 +11,98 @@ function BigTable(){
     function bindEvent(){
         $(".J_addBigRow").live("click", function(){
             var html = $("#J_bigRowTemplate").html().toString();
+            var next_id = $("#J_nextId").attr("next-id");
+
+            html = html.replace(/ J_template/,"");
+            for(var i = 0 ;i < 3 ;i++){
+                html = html.replace(/data-id=\"\"/, "data-id=\""+next_id+"\"");
+                next_id = parseInt(next_id)+1;
+                }
+
+            $(this).closest("#J_bigTable").append(html);
+            $("#J_nextId").attr("next-id", next_id);
+        
+        });
+
+        $(".J_delBigRow").live("click", function(){
+            var length = $("#J_bigTable tbody").length;
+
+            if (length != 2 )
+                $(this).closest(".J_bigRow").remove();
+
+        });
+
+        $(".J_addSmallRow").live("click", function(){
+            var data_id = $(this).attr("data-id");
+            var next_id = $("#J_nextId").attr("next-id");
+            var count = $(".J_smallTable").html().toString();
+            var smallTable = $("<tr data-id=\""+next_id+"\" class=\"J_smallTable\"></tr>").html(count);
+
+            $(this).closest(".J_bigRow").append(smallTable);
+            next_id = parseInt(next_id)+1;
+            $("#J_nextId").attr("next-id", next_id);
+        });
+
+        $(".J_delSmallRow").live("click", function(){
+            var bigRow = $(this).closest(".J_bigRow");
+            var length = bigRow.children("tr").length;
+            var row_span = parseInt(length)-1;
+
+            if (length > 2)
+                    $(this).closest(".J_smallTable").remove();
+
+            bigRow = bigRow.html().replace(/rowspan=\"4\"/g,"rowspan=\""+row_span+"\"");
+            console.log(bigRow);         
+        
+        });
+
+        $("#J_test").bind("click", function(){
+            getPlanData();
+        });
+    }
+
+    function getPlanData(){
+        var data = [];
+        $.each($(".J_bigRow"), function(index, value){
+            var $value = $(value);
+            if(!$value.hasClass("J_template")){
+
+                var item = {};
+
+                item.color_name = $(value).find(".J_colorName").val();
+                item.color_number = $(value).find(".J_colorNumber").val();
+                item.gang_number = $(value).find(".J_gangNumber").val();
+                item.spec = [];
+
+                $.each($value.find(".J_sizeTable tr"), function(idx, val){
+                    item.spec.push({size:$(val).find("input").val(), count:null});
+                });
+
+                $.each($value.find(".J_countTable tr"), function(idx, val){
+                    item.spec[idx].count = $(val).find("input").val();
+                });
+
+                data.push(item);
+            }
+        });
+
+        console.log(data);
+    }
+}
+/*
+$(document).ready(function(){
+    var bigtable = new BigTable();
+    bigtable.init();
+});
+
+function BigTable(){
+    this.init = function(){
+        bindEvent();
+    };
+
+    function bindEvent(){
+        $(".J_addBigRow").live("click", function(){
+            var html = $("#J_bigRowTemplate").html().toString();
             var next_id = $(this).attr("next-id");
 
             html = html.replace(/ J_template/,"");
@@ -71,3 +163,4 @@ function BigTable(){
         console.log(data);
     }
 }
+*/
--- protected/views/plan/createPlanList.php
+++ protected/views/plan/createPlanList.php
@@ -1,4 +1,110 @@
+<h2 class="prepend-1">创建生产计划</h2>
+<hr>
+
+<div class="contant-container prepend" id="createPlanList">
 客户：
+<a href="<?php echo $this->baseUrl; ?>/ajaxStorage/selectprovider" id="J_selectProvider" provider="none">点击选择客户</a>
+
+货号：<input type="text"></input>
+针型：<input type="text"></input>
+<table class="record" id="J_bigTable" >
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td>颜色</td>
+            <td>色号</td>
+            <td>缸号</td>
+            <td>尺码</td>
+            <td>件数</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+    <tbody class="J_bigRow">
+        <tr>
+            <td rowspan="4"><button class="J_addBigRow">添加</button></td>
+            <td rowspan="4"><button class="J_delBigRow">删除</button></td>
+            <!-- 颜色  -->
+            <td rowspan="4"><input type="text" class="J_colorName"></input></td>
+            <!-- 色号  -->
+            <td rowspan="4"><input type="text" class="J_colorNumber"></input></td>
+            <!-- 缸号  -->
+            <td rowspan="4"><input type="text" class="J_gangNumber"></input></td>
+
+        </tr>
+        <tr class="J_smallTable" data-id="1">
+            <!-- 尺码  -->
+            <td class="J_sizeTable"><input type="text"></input></td>
+            <!-- 件数  -->
+            <td class="J_countTable"><input type="text"></input></td>
+            <td><button class="J_addSmallRow">添加</button></td>
+            <td><button class="J_delSmallRow">删除</button></td>
+        </tr>
+        <tr class="J_smallTable" data-id="2">
+            <!-- 尺码  -->
+            <td class="J_sizeTable"><input type="text"></input></td>
+            <!-- 件数  -->
+            <td class="J_countTable"><input type="text"></input></td>
+            <td><button class="J_addSmallRow">添加</button></td>
+            <td><button class="J_delSmallRow">删除</button></td>
+        </tr>
+        <tr class="J_smallTable" data-id="3">
+            <!-- 尺码  -->
+            <td class="J_sizeTable"><input type="text"></input></td>
+            <!-- 件数  -->
+            <td class="J_countTable"><input type="text"></input></td>
+            <td><button class="J_addSmallRow">添加</button></td>
+            <td><button class="J_delSmallRow">删除</button></td>
+        </tr>
+    </tbody>
+</table>
+
+<span id="J_nextId" next-id="4"></span>
+<span id="J_rowspan" rowspan="4"></span>
+
+<table class="hide" id="J_bigRowTemplate" >
+    <tbody class="J_bigRow">
+        <tr class="J_template">
+            <td rowspan="4"><button class="J_addBigRow">添加</button></td>
+            <td rowspan="4"><button class="J_delBigRow">删除</button></td>
+            <!-- 颜色  -->
+            <td rowspan="4"><input type="text" class="J_colorName"></input></td>
+            <!-- 色号  -->
+            <td rowspan="4"><input type="text" class="J_colorNumber"></input></td>
+            <!-- 缸号  -->
+            <td rowspan="4"><input type="text" class="J_gangNumber"></input></td>
+        </tr>
+        <tr class="J_smallTable" data-id="">
+            <!-- 尺码  -->
+            <td class="J_sizeTable"><input type="text"></input></td>
+            <!-- 件数  -->
+            <td class="J_countTable"><input type="text"></input></td>
+            <td><button class="J_addSmallRow">添加</button></td>
+            <td><button class="J_delSmallRow">删除</button></td>
+        </tr>
+        <tr class="J_smallTable" data-id="">
+            <!-- 尺码  -->
+            <td class="J_sizeTable"><input type="text"></input></td>
+            <!-- 件数  -->
+            <td class="J_countTable"><input type="text"></input></td>
+            <td><button class="J_addSmallRow">添加</button></td>
+            <td><button class="J_delSmallRow">删除</button></td>
+        </tr>
+        <tr class="J_smallTable" data-id="">
+            <!-- 尺码  -->
+            <td class="J_sizeTable"><input type="text"></input></td>
+            <!-- 件数  -->
+            <td class="J_countTable"><input type="text"></input></td>
+            <td><button class="J_addSmallRow">添加</button></td>
+            <td><button class="J_delSmallRow">删除</button></td>
+        </tr>
+    </tbody>
+</table>
+
+<button id="J_test">get data</button>
+</div>
+<!--客户：
 <a href="<?php echo $this->baseUrl; ?>/ajaxStorage/selectprovider" id="J_selectProvider" provider="none">点击选择客户</a>
 
 货号：<input type="text"></input>
@@ -18,13 +124,13 @@
         <tr class="J_bigRow">
             <td><button class="J_addBigRow" next-id="4">添加</button></td>
             <td><button class="J_delBigRow" next-id="4">删除</button></td>
-            <!-- 颜色  -->
+            <!-- 颜色  --
             <td><input type="text" class="J_colorName"></input></td>
-            <!-- 色号  -->
+            <!-- 色号  --
             <td><input type="text" class="J_colorNumber"></input></td>
-            <!-- 缸号  -->
+            <!-- 缸号  --
             <td><input type="text" class="J_gangNumber"></input></td>
-            <!-- 尺码  -->
+            <!-- 尺码  --
             <td>
                 <table>
                     <tbody class="J_sizeTable">
@@ -40,7 +146,7 @@
                     </tbody>
                 </table>
             </td>
-            <!-- 件数  -->
+            <!-- 件数  --
             <td>
                 <table>
                     <tbody class="J_countTable">
@@ -83,13 +189,13 @@
         <tr class="J_bigRow J_template">
             <td><button class="J_addBigRow" data-id="" next-id="">添加</button></td>
             <td><button class="J_delBigRow" data-id="" next-id="">删除</button></td>
-            <!-- 颜色  -->
+            <!-- 颜色  --
             <td><input type="text" class="J_colorName"></input></td>
-            <!-- 色号  -->
+            <!-- 色号  --
             <td><input type="text" class="J_colorNumber"></input></td>
-            <!-- 缸号  -->
+            <!-- 缸号  --
             <td><input type="text" class="J_gangNumber"></input></td>
-            <!-- 尺码  -->
+            <!-- 尺码  --
             <td>
                 <table>
                     <tbody>
@@ -99,7 +205,7 @@
                     </tbody>
                 </table>
             </td>
-            <!-- 件数  -->
+            <!-- 件数  --
             <td>
                 <table>
                     <tbody>
@@ -123,4 +229,4 @@
     </tbody>
 </table>
 
-<button id="J_test">get data</button>
+<button id="J_test">get data</button>-->
