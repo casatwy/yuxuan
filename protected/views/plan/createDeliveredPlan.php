7f236646a97bc89c3c32aaffd92550c738bc7ca7
winni <winni@winni.com>
2013-02-27 00:44:38 +0800

    color

--- protected/views/plan/createDeliveredPlan.php
+++ protected/views/plan/createDeliveredPlan.php
@@ -1,5 +1,5 @@
-<h2 class="prepend-1 span-16">新增外发计划</h2>
-<span class="span-3"><button id="J_saveAll">保存全部</button></span>
+<h2 class="prepend-1 span-18">新增外发计划</h2>
+
 <hr>
 <div class="contant-container">
     <span class="span-15">
@@ -16,6 +16,8 @@
             
         </div>
     </div>
+    <hr>
+    <span class="prepend-11 span-3"><button id="J_saveAll">保存全部</button></span>
 </div>
 <div class="hide" id="J_addDeliverdPlan">
     <div class="J_deliverdPlan">
