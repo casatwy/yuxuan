c7b7158c659de007978facf24d26af561e684f11
casa <casa@yuxuan-server.(none)>
2013-03-03 13:12:37 +0800

    fixed a bug on create new item

--- js/common/StockHelper.js
+++ js/common/StockHelper.js
@@ -19,9 +19,7 @@ function StockHelper(baseUrl){
                 $.jGrowl("请先填写货号.", { header:"提示", life: 5000 });
                 success = false; 
                 return false;
-                }
-
-            else {
+            }else{
                 $(valContent).find(".J_item").each(function(index, value){
                     $(value).find("input").each(function(ind, val){
                         if($(val).val().length == 0){
