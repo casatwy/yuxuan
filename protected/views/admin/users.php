c40ca422305fb5e7c0368457638e1629f7ac0fd9
CasaTaloyum <casatwy@msn.com>
2013-03-08 21:11:01 +0800

    commited for bug #55 #54

--- protected/views/admin/users.php
+++ protected/views/admin/users.php
@@ -8,15 +8,21 @@
         <th>姓名</th>
         <th>电话</th>
         <th>权限</th>
-        <th>操作</th>
+<?php
+//        <th>操作</th>
+?>
     </tr>
 	<?php foreach($users as $user): ?>
 	<tr>
         <td><?php echo $user->name;?></td>
 		<td><?php echo  $user->telephone;?></td>
 		<td><?php echo  $user->authority;?></td>
+<?php
+/*
         <td><a href="<?php echo $this->baseUrl.'/admin/updateUser/id/'.$user->id;?>" class="J_update">修改信息</a>
         &nbsp;<a href="<?php echo $this->baseUrl.'/admin/deleteUser/id/'.$user->id;?>" >删除</a></td>
+ */
+?>
 	</tr>
 	<?php endforeach; ?>
 </table>
