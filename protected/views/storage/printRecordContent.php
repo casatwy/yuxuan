51b66617838a9ad71f9c1ac4b6ddbde5dc899349
CasaTaloyum <casatwy@msn.com>
2013-03-31 14:51:57 +0800

    changed to 15

--- protected/views/storage/printRecordContent.php
+++ protected/views/storage/printRecordContent.php
@@ -1,5 +1,5 @@
 <?php
-define("RECORD_TABLE_ROW_LIMIT", 10);
+define("RECORD_TABLE_ROW_LIMIT", 12);
 $recordListArray = array_chunk($recordList, RECORD_TABLE_ROW_LIMIT);
 $totalPageCount = ceil(count($recordList) / RECORD_TABLE_ROW_LIMIT);
 foreach($recordListArray as $pageNumber => $recordListItem):
