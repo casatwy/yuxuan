277739e1bf4365e41bdb9aa23d4fbead256b41e9
Casa Taloyum <casatwy@msn.com>
2013-04-05 21:29:58 +0800

    removed the duplicate datepicker

--- js/common/RecordHelper.js
+++ js/common/RecordHelper.js
@@ -11,17 +11,6 @@ function RecordHelper(baseUrl){
     }
 
     function bindEvent(){
-        $(".J_selectTime").datepicker({
-            autoSize:true,
-            dateFormat:"yy-mm-dd",
-            dayNamesMin:["日","一","二","三","四","五","六"],
-            monthNamesShort:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
-            changeMonth:true,
-            changeYear:true,
-            showAnim:"fadeIn",
-            showMonthAfterYear:true,
-        });
-
         setAccordion(false);
 
         $("#J_searchButton").bind("click", function(){
